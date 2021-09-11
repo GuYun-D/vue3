b386c106148c5e0ad0e1bf44115807996783320d
GuYun-D <1946306647@qq.com>
2021-09-11 22:06:24 +0800

    webpack的import函数实现分包

--- vue3components/src/main.js
+++ vue3components/src/main.js
@@ -1,4 +1,12 @@
 import { createApp } from 'vue'
-import App from './09 - 动态组件/App.vue'
+import App from './10 - 异步组件/App.vue'
+
+/**
+ * 通过import函数导入的模块，webpack对其打包的时候就会进行分包
+ */
+import('./10 - 异步组件/utils/tools').then(res => {
+  console.log(res);
+  console.log(res.add(10, 50));
+})
 
 createApp(App).mount('#app')
