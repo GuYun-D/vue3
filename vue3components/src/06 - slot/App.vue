dce6d362a47c0adeb86c98555f937f0c3837a1be
GuYun-D <1946306647@qq.com>
2021-09-11 16:36:21 +0800

    插槽的使用

---
+++ vue3components/src/06 - slot/App.vue
@@ -0,0 +1,27 @@
+<template>
+  <div>
+    <MySlotCpn>
+      <button>我是一个按钮</button>
+    </MySlotCpn>
+
+    <MySlotCpn>
+      <my-btn>我是一个按钮</my-btn>
+    </MySlotCpn>
+  </div>
+</template>
+
+<script>
+import MySlotCpn from "./MySlotCpn";
+import MyBtn from "./MyBtn";
+
+export default {
+  name: "App",
+  components: {
+    MySlotCpn,
+    MyBtn
+  },
+};
+</script>
+
+<style scoped>
+</style>
--- vue3components/src/main.js
+++ vue3components/src/main.js
@@ -1,4 +1,4 @@
 import { createApp } from 'vue'
-import App from './05 - eventBus/App.vue'
+import App from './06 - slot/App.vue'
 
 createApp(App).mount('#app')
